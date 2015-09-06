113953498e21776c638f91eddba785ec405404e3
Ryan Davis <ryan@acceleration.net>
2015-09-05 21:46:48 -0400

    use `valueLink` instead of `ref`
    Largely based on
    http://stackoverflow.com/questions/29503213/use-state-or-refs-in-react-js-form-components
    
    Is a little simpler, one fewer concept/system to wrangle with

--- src/KpiAddDataPoint.jsx
+++ src/KpiAddDataPoint.jsx
@@ -1,25 +1,28 @@
 'use strict'; // -*- mode:js2 -*-
 
-var React = require('react'),
+var React = require('react/addons'),
     bs = require('react-bootstrap'),
     eventBus = require('./EventBus.js')
 ;
 
 module.exports = React.createClass({
+  mixins: [React.addons.LinkedStateMixin],
+  getInitialState: function() { return {value: 0}; },
   add: function(e) {
     e.preventDefault();
     eventBus.publish(eventBus.events.ADD_DATAPOINT, {
       kpi: this.props.kpi,
       point: {
         utime: new Date().getTime(),
-        value: parseFloat(this.refs.amount.getValue())
+        value: parseFloat(this.state.value)
       }
     });
+    this.setState(this.getInitialState());
   },
   render: function() {
     var addBtn = <bs.ButtonInput bsStyle="primary" type="button" onClick={this.add}>Add</bs.ButtonInput>;
     return (
-            <bs.Input type="number" buttonAfter={addBtn} ref="amount"/>
+        <bs.Input type="number" buttonAfter={addBtn} valueLink={this.linkState('value')} />
     );
   }
 });
--- src/KpiForm.jsx
+++ src/KpiForm.jsx
@@ -1,14 +1,12 @@
 'use strict'; // -*- mode:js2 -*-
 
-var React = require('react'),
+var React = require('react/addons'),
     bs = require('react-bootstrap'),
     eventBus = require('./EventBus.js')
 ;
 module.exports = React.createClass({
+  mixins: [React.addons.LinkedStateMixin],
   getInitialState: function() { return {name: ''}; },
-  onChange: function() {
-    this.setState({name: this.refs.name.getValue()});
-  },
   add: function(e) {
     e.preventDefault();
     if(!this.state.name) return;
@@ -22,7 +20,8 @@ module.exports = React.createClass({
     return (
       <div>
         <h2>Add KPI</h2>
-        <bs.Input type="text" value={this.state.name} placeholder="name" ref="name" onChange={this.onChange} buttonAfter={addBtn} label="KPI Name" />
+        <bs.Input type="text" placeholder="name" valueLink={this.linkState('name')}
+            buttonAfter={addBtn} label="KPI Name" />
         </div>
     );
   }
